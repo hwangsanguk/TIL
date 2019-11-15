e085f42854fceaa17232d5fa350f2b1c437f5f0e
tony <lall21@naver.com>
2019-11-15 15:20:48 +0900

    [ADD]:추가

--- day08/component/src/index.js
+++ day08/component/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+import Segment from './components/Segment';
+import Message from './components/Message';
+import CommentDetail from './components/CommentDetail';
+import ApprovalCard from './components/ApprovalCard';
+
 
 class App extends React.Component{
     // constructor
@@ -9,7 +14,48 @@ class App extends React.Component{
     state = {
         comment:[],
         number: 0,
-        infomation: faker.lorem.paragraph()
+        information: faker.lorem.paragraph()
+    };
+
+    handleAddComment = () =>{
+        const newComment = {
+            author : faker.name.firstName(),
+            time : faker.date.recent().toLocaleString(),
+            body: faker.lorem.sentence(),
+            avatar : faker.image.avatar(),
+        }
+        this.setState({
+            comments: [newComment, ...this.state.comment]
+        });
+
+    };
+    handlePlus = () =>{
+        this.setState({ number: this.state.number +1
+        });
+
+    };
+    render(){
+        return(
+            <>
+                <span>{this.state.number}</span>
+                <button onClick={this.handlePlus}>plus</button>
+
+                <Segment>
+                    <div className="ui icon header">
+                    <i className="pdf file outline icon">
+                        No document
+                    </i>
+                    </div>
+                    <div className="ui primary button">Add document</div>
+                </Segment>
+                <Segment>
+                    <h4 className="ui header">For your information</h4>
+                    <p>{this.state.information}</p>
+                </Segment>
+                <Message header="회원 약관 변경" body="약관이 변경되었습니다" />
+
+            </>
+        )
     };
 };
 
