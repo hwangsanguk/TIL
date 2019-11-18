f95178e574697f113dc776e3205909ff7f170f53
tony <lall21@naver.com>
2019-11-18 10:25:23 +0900

    초기설정

--- day09/season-checker/src/index.js
+++ day09/season-checker/src/index.js
@@ -10,6 +10,10 @@ const App = ()=>{
         <div>계절 확인</div>
     );
 };
+//클래스형 컴포넌트
+//1. JS class 를 사용
+//2. extends React.Component
+//3. render(){return(JSX template)}
 
 ReactDom.render(<App />,
 document.querySelector('#root'));
