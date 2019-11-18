81810763e12087e53ff34b568f1c20fae91db1dd
tony <lall21@naver.com>
2019-11-18 11:31:33 +0900

    [MOD]

--- day09/season-checker/src/index.js
+++ day09/season-checker/src/index.js
@@ -1,21 +1,103 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const App = ()=>{
-    window.navigator.geolocation.getCurrentPosition(
-        (position) =>{ console.log(position)},
-        (error) => {console.log(error)}
+//함수형 컴포넌트
+// const App = ()=>{
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) =>{ console.log(position)},
+//         (error) => {console.log(error)}
+//     )
+//     return(
+//         <div>계절 확인</div>
+//     );
+// };
+
+//위도를 넘겨 준다 => props 사용
+
+//SeasonDisplay 사용해서 위도를 출력
+
+const SeasonDisplay = (props) =>{
+    return (
+        //JSX문법
+        <div>
+
+        </div>
     )
-    return(
-        <div>계절 확인</div>
-    );
-};
+
+}
+
+
 //클래스형 컴포넌트
 //1. JS class 를 사용
 //2. extends React.Component
 //3. render(){return(JSX template)}
+class App extends React.Component {
+    //constructor 에서 하는 일 이 state설정 뿐일 때, 생략이 가능함
+    
+    constructor(props) {
+        super(props);
+
+        //오브젝트 형태로 들어가야함 key value
+        //state눈 컴포넌트 안에서만 사용됨
+        //컴포넌트가 생성될 때, 단 1번만
+        //setState로만 데이터 수정
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        }
+    };
+    componentDidMount(){
+        console.log("컴포넌트가 화면에 렌더링이 끝났따")
+    }
+    componentDidUpdate(){
+        console.log('컴포넌트가 업데이트가 되고, 리렌더링이 끝났다')
+    }
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                 console.log(position)
+                this.setState({lat :position.coords.latitude})
+                 },
+            (error) => { console.log(error)
+            this.setState({
+                errorMessage:error.message
+            }) }
+        );
+        //조건부 렌더링
+
+        //허용
+        if (this.state.lat && !this.state.errorMessage){
+            return(
+                <div>
+                    위도: {this.state.lat}
+                </div>
+            )
+        };
+        //거절
+        if(!this.state.lat && this.state.errorMessage){
+            return(
+                <div>
+                    {this.state.errorMessage}
+                    사용자 위치 정보가 필요합니다 
+                </div>
+            )
+        }
+        return (
+            <div>
+                사용자 위치 정보 동의해주세요
+            </div>);
+    }
+}
 
 ReactDom.render(<App />,
-document.querySelector('#root'));
+    document.querySelector('#root'));
+
+    //life cycle method
+
+    //constructor() {단 1번 실행되면 좋은 코드들}
+    //render() {return JSX Template}
+    //componentDidMount(){ 컴포넌트가 화면에 렌더링이 끝나면}
+    //componentDidUpdate(){컴포넌트가 업데이트가되 고, 리렌더링이 끝나면}
+    //componentWillUnmount()
 
 
